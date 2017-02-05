7480e2212f99dfe70f870e72ee78190fb92b66aa
MarianIakubets <marian.iakubets@perfectial.com>
2017-02-05 13:27:14 +0200

    2

--- src/cordova.js
+++ src/cordova.js
@@ -4,7 +4,7 @@ document.addEventListener('deviceready', onDeviceReady);
 function onDeviceReady(){
     // my game
     AdMob.createBanner({
-        adId: "ca-app-pub-2062493463858889/7200177030", // AdMob banner ID
+        adId: "ca-app-pub-6362858486849834/5867906107", // AdMob banner ID
         position: AdMob.AD_POSITION.BOTTOM_CENTER, // position
         autoShow: true, // show it once it's ready
         overlap:true, // overlap the game, so it won't steal space to canvas
